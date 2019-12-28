ecca3780758d5a4403ed3a12f919639091abbfca
AudreyHal <chiomahalim@gmail.com>
2019-12-28 16:43:10 +0100

    Include scripts and add loader

---
+++ assets/scripts/index.js
@@ -0,0 +1,28 @@
+$(document).ready(()=>{
+  // $('.first-load-container').ripples({
+  //   resolution: 512,
+  //   dropRadius: 20,
+  //   perturbance: 0.04,
+    
+  // });
+
+  console.log("b0oom")
+  // setTimeout(function() {
+    
+  // }, timeout);
+
+  // const controller = new ScrollMagic.Controller();
+
+  // let scene=new ScrollMagic.Scene({
+  //   triggerElement:'',
+  //   duration:'',
+  //   triggerHook:0,
+  //   offset:'300'
+  // })
+  // .setTween()
+  // .setPin()
+  // .addTo(controller)
+
+
+
+})
